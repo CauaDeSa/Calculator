9f3d3ccaf4447bc3acf6701e2284b3569a6383f8
Cauã Rufino de Sá <cauarufinodesa@gmail.com>
2024-04-18 16:41:55 -0300

    Do while loppings to filter inputs and receiving the operand character.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,14 +1,30 @@
 ﻿float n1, n2;
+char operation;
 
 Console.Write("Type number 1: ");
 n1 = int.Parse(Console.ReadLine());
 
-Console.Write("Type number 2: ");
-n2 = int.Parse(Console.ReadLine());
+do {
+    Console.Write("Type number 2 (cannot be 0): ");
+    n2 = int.Parse(Console.ReadLine());
+} while (n2 == 0);
 
-Console.WriteLine($"Summed: {n1 + n2}");
-Console.WriteLine($"Subtracted: {n1 - n2}");
-Console.WriteLine($"Multiplied: {n1 * n2}");
-Console.WriteLine($"Divided: {n1 / n2}");
+do
+{
+    Console.Write("Type operation ('x' or '/' or '+' or '-'):  ");
+    operation = char.Parse(Console.ReadLine());
+} while (operation != '+' && operation != '-' && operation != 'x' && operation != '/');
+
+if (operation == '+')
+    Console.WriteLine($"Summed: {n1 + n2}");
+else 
+    if (operation == '-')
+        Console.WriteLine($"Subtracted: {n1 - n2}");
+    else
+        if (operation == 'x')
+            Console.WriteLine($"Multiplied: {n1 * n2}");
+        else
+            if (operation == '/')
+                Console.WriteLine($"Divided: {n1 / n2}");
 
 Console.ReadKey();
