77d511b52b5966937291a360620cb8088058a093
Cauã Rufino de Sá <cauarufinodesa@gmail.com>
2024-04-19 10:20:53 -0300

    getting 4 numbers instead of 2 and testing function getNumber (commented)

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,13 +1,29 @@
-﻿float n1, n2;
+﻿float n1, n2, n3, n4;
 char operation;
 
 Console.Write("Type number 1: ");
 n1 = int.Parse(Console.ReadLine());
 
+//n2 = getNumber(2);
+//n3 = getNumber(3);
+//n3 = getNumber(4);
+
 do {
-    Console.Write("Type number 2 (cannot be 0): ");
+    Console.Write("Type number 2 (cant be 0): ");
     n2 = int.Parse(Console.ReadLine());
 } while (n2 == 0);
+
+do
+{
+    Console.Write("Type number 3 (cant be 0): ");
+    n3 = int.Parse(Console.ReadLine());
+} while (n3 == 0);
+
+do
+{
+    Console.Write("Type number 4 (cant be 0): ");
+    n4 = int.Parse(Console.ReadLine());
+} while (n4 == 0);
 
 do
 {
@@ -16,15 +32,22 @@ do
 } while (operation != '+' && operation != '-' && operation != 'x' && operation != '/');
 
 if (operation == '+')
-    Console.WriteLine($"Summed: {n1 + n2}");
-else 
-    if (operation == '-')
-        Console.WriteLine($"Subtracted: {n1 - n2}");
-    else
-        if (operation == 'x')
-            Console.WriteLine($"Multiplied: {n1 * n2}");
+    Console.WriteLine($"Summed: {n1 + n2 + n3 + n4}");
+else if (operation == '-')
+        Console.WriteLine($"Subtracted: {n1 - n2 -n3 - n4}");
+    else if (operation == 'x')
+            Console.WriteLine($"Multiplied: {n1 * n2 * n3 * n4}");
         else
-            if (operation == '/')
-                Console.WriteLine($"Divided: {n1 / n2}");
+            Console.WriteLine($"Divided: {n1 / n2 / n3 / n4}");
 
 Console.ReadKey();
+
+//float getNumber(int option) {
+//    float result;
+//    do
+//    {
+//        Console.Write($"Type number {option} (cant be 0): ");
+//        result = int.Parse(Console.ReadLine());
+//    } while (result == 0);
+//    return result;
+//}
